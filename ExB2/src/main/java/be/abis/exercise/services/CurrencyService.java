bc3512b13768433741c594858231a577db2aa2c9
Sandy <sschillebeeckx@abis.be>
2020-10-15 15:29:08 +0200

    B

---
+++ ExB2/src/main/java/be/abis/exercise/services/CurrencyService.java
@@ -0,0 +1,9 @@
+package be.abis.exercise.services;
+
+public interface CurrencyService {
+	
+	//public double getExchangeRate(String fromCur, String toCur);
+
+	double getExchangeRate(String toCur);
+
+}
